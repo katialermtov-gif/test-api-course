4e96661fd37cfc994d5e24a061a485e90a2c3cdb
katialermtov-gif <katialermtov@gmail.com>
2026-06-10 13:48:54 +0200

    Add files via upload

---
+++ домашка7/test.js
@@ -0,0 +1,88 @@
+
+
+// @ts-chek
+// console.log("Hello, World!");
+// console.log("Hello, World!");
+// console.log("Hello, World!");
+// console.log("Hello, World!");
+// console.log("Hello, World!");
+// var body = "Hello World12345556"
+// var body2 = 1234
+// console.log(body)
+
+
+
+// var a = 1 
+// a = 2
+// var b = "hfhf"
+// const c = 123345
+
+// console.log(a)
+
+
+
+
+
+// let numbers = [4,4,8,3,3,2,4,4];
+// console.log (numbers.length)
+// numbers.forEach(asd => {
+//   console.log(asd);
+// })
+
+
+
+
+
+
+// let numbers = [4,4,8,3,3,2,4,4];
+// console.log(numbers[0], numbers[1], numbers[2]);
+
+
+
+
+
+
+// let numbers = [4,4,8,3,3,2,1,4,4];
+// let summa = 0;
+// for (let i = 0; i < numbers.length; i++) {
+//   summa = summa + numbers[i];
+//   console.log(summa)
+// }
+
+// console.log('result: ', summa);
+
+
+
+
+
+// let numbers = [4,4,8,3,3,2,1,4,4];
+// let summa = 0;
+// for (let i = 0; i < numbers.length; i++) {
+//   if (numbers[i] !== 4) {
+//     summa = summa + numbers[i];
+//   }
+//   console.log(summa)
+// }
+
+// console.log('result: ', summa);
+
+
+
+
+
+
+// const fs = require("fs");
+
+// let file = fs.readFileSync("list.json", "utf-8");
+// let data = JSON.parse(file);
+
+// let lists = data.lists;
+
+// lists.forEach(list => {
+//   console.log(`ID: ${list.id}, Name: ${list.name}`);
+// });
+
+
+
+
+
